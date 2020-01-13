54fa413c533100f20b6f0d9ff5ffaf7a8455b868
enid svymbersky <enid.bersky@gmail.com>
2020-01-13 19:11:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,11 +25,13 @@ return ("done");
 
 
 function doWhileLoop(num) {
-var i = 0
+  var i = 0;
+ 
 function incrementVariable() {
   i = i + 1;
   return i;
 }
+ 
 do {
   console.log("I run once regardless.");
 } while (incrementVariable() < num);
