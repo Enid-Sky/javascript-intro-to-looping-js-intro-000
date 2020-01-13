96a5072628a5c7a0ae32a417f1e45ff03acb7c48
enid svymbersky <enid.bersky@gmail.com>
2020-01-13 18:43:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,12 +4,19 @@
 //return array
 
 function forLoop (array) {
-  for(let i = 0 ; i < 25; i++) {
-    let current = array[i];
-    if(current === 1) {
-      return `I am 1 strange loop`;
+  for(var i = 0 ; i < 25; i++) {
+    if(i === 1) {
+    array.push("I am 1 strange loop.");
     } else {
-      return `I am ${i} strange loops.`;
+    array.push(`I am ${i} strange loops.`);
     }
   }
+  return array;
+}
+
+function whileLoop(number){
+while (number > 0) {
+  console.log(--number);
+}
+return ("done");
 }
